d4f38b7ac3860d03e0b99efd5281a9c8c244ff2f
patrickpca <81128104+patrickpca@users.noreply.github.com>
2022-05-08 17:28:00 -0300

    Create portscan.ps1

---
+++ Portscan.ps1
@@ -0,0 +1,14 @@
+param($ip)
+if(!$ip){
+    echo "PORTSCAN"
+    echo ".\portscan.ps1 192.168.0.1 80"
+}
+else{
+$topports = 21,22,3306,80,443
+try {foreach ($porta in $topports){
+if (Test-NetConnection $ip -Port $porta -WarningAction SilentlyContinue -InformationLevel Quiet) 
+{
+echo "Porta $porta Aberta"
+}} else {
+echo "Porta $porta Fechada"
+}} catch {}
